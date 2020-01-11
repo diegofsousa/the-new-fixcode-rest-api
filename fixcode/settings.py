28aa8a682e8222ad741e7f9e1edf1d6404b3ba6a
diegofsousa <diegofernando5672@gmail.com>
2020-01-11 20:32:47 -0300

    [#1, #3] - A User entity created, 'requirements.txt' updated

--- fixcode/settings.py
+++ fixcode/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'fixcode.user',
+
+    # thirty apps
+    'easy_thumbnails'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'user.User'
